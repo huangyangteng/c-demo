20cc4918f1ac41b68feee931dede18dd3d356cd9
yellowyellow <yellowyellow12138@gmail.com>
2017-06-06 11:01:13 +0800

    宏定义+数据类型+舍汉王

--- runoob/hongdingyi.c
+++ runoob/hongdingyi.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 #define URL "http://www.fishc.com"
-#define NAME "我"
+#define NAME "鱼c工作室"
+#define BOSS "小甲鱼"
 #define YEAR 2010
-
+#define MONTH 5
+#define DAY 20
 int main(){
-	printf("%d\n",YEAR);
+	printf("%s成立于%d年%d月%d日\n",NAME,YEAR,MONTH,DAY);
+	printf("%s是%s创立的\n", NAME,BOSS);
+	printf("%s的域名是%s\n",NAME,URL );
+	
 	return 0;
 }
