86b6de1f6e6668e339178df0a0c6966d836efb5a
yellowyellow <yellowyellow12138@gmail.com>
2017-06-06 10:10:04 +0800

    开始+变量+常量

---
+++ c-practice/c-practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-practice
+//
+//  Created by best9 on 2017/4/7.
+//  Copyright © 2017年 best9. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ runoob/hongdingyi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define URL "http://www.fishc.com"
+#define NAME "我"
+#define YEAR 2010
+
+int main(){
+	printf("%d\n",YEAR);
+	return 0;
+}
